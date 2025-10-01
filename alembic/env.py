fc049acdecbd7c54eb9ee48a48715c7b582cd880
Obed Domínguez Mora <pacocobot2@gmail.com>
2025-09-30 19:59:20 -0600

    Add: database url into alembic env file

--- alembic/env.py
+++ alembic/env.py
@@ -5,9 +5,21 @@ from sqlalchemy import pool
 
 from alembic import context
 
+from sqlmodel import SQLModel
+
+from database.connection import engine
+from database.models.models import User
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DATABASE_URL = os.getenv('DATABASE_URL')
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+config.set_main_option("sqlalchemy.url", DATABASE_URL)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
@@ -18,7 +30,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = SQLModel.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
