f1196c07d1dd15de53ba4cd4774e10f68a5cc404
Obed Domínguez Mora <pacocobot2@gmail.com>
2025-09-30 20:03:21 -0600

    Add migration: update user email lenght to 254 char

--- database/models/models.py
+++ database/models/models.py
@@ -7,7 +7,7 @@ class User(SQLModel, table=True):
     user_id: str = Field(default_factory=lambda: str(uuid.uuid7()), primary_key=True, index=True, unique=True)
     name: str
     surname: str
-    email: str = Field(sa_column=Column(String(100), unique=True, index=True, nullable=False))
+    email: str = Field(sa_column=Column(String(254), unique=True, index=True, nullable=False))
     age: Optional[int] = Field(default=None)
     weight: Optional[float] = Field(sa_column=Column(Numeric(5, 2), default=None))
     height: Optional[float] = Field(sa_column=Column(Numeric(3, 2), default=None))
