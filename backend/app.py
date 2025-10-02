f48ad488ce4e271977f90a63f1031403c61eed59
Obed Domínguez Mora <pacocobot2@gmail.com>
2025-10-02 00:59:52 -0600

    Fix: routes import

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,8 @@
-#run with: uvicorn app:app --reload --port 8000
+#run with: uvicorn backend. app:app --reload --port 8000
 
 from fastapi import FastAPI
 from dotenv import load_dotenv
-from routes.routes import router
+from backend.routes.routes import router
 import os
 
 load_dotenv()
