75656884ac8c55a528ab3bfb504833e80fc449c9
Obed Domínguez Mora <pacocobot2@gmail.com>
2025-10-01 20:57:24 -0600

    Add migration: Create User_Role table as an intermediary table for User and Roles

--- database/models/models.py
+++ database/models/models.py
@@ -1,7 +1,11 @@
 import uuid
-from sqlmodel import SQLModel, Field
-from typing import Optional
+from sqlmodel import SQLModel, Field, Relationship
+from typing import Optional, List
 from sqlalchemy import String, Column, Numeric, CheckConstraint
+
+class User_Role(SQLModel, table=True):
+    user_id: str = Field(foreign_key="user.user_id", primary_key=True)
+    role: str = Field(foreign_key="role.role", primary_key=True)
 
 class User(SQLModel, table=True):
     user_id: str = Field(default_factory=lambda: str(uuid.uuid7()), primary_key=True, index=True, unique=True)
@@ -16,5 +20,9 @@ class User(SQLModel, table=True):
         CheckConstraint('age >= 0 AND age <= 120', name='age_to_120'),
     )
 
+    roles: List["Role"] = Relationship(back_populates="users", link_model=User_Role)
+
 class Role(SQLModel, table=True):
     role: str = Field(sa_column=Column(String(12), primary_key=True, index=True, unique=True))
+
+    users: List[User] = Relationship(back_populates="roles", link_model=User_Role)
