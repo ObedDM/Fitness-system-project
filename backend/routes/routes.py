3fe9b849cb7327f60591a7ec8fa2d1728e820da3
Obed Domínguez Mora <pacocobot2@gmail.com>
2025-10-02 01:00:33 -0600

    Create: register_user and get_users endpoints to test out SQLmodel integration

--- backend/routes/routes.py
+++ backend/routes/routes.py
@@ -1,4 +1,9 @@
-from fastapi import APIRouter
+#run with: uvicorn backend.app:app --reload --port 8000
+
+from fastapi import APIRouter, Depends
+from database.connection import get_session
+from database.models.models import User, User_Role, Role
+from sqlmodel import Session, select
 
 router = APIRouter()
 
@@ -9,3 +14,17 @@ async def root():
 @router.get('/hola')
 async def hola():
     return {'hola': 12543}
+
+@router.post('/register_user')
+async def register_user(user: User, session: Session = Depends(get_session)):
+    session.add(user)
+    session.commit()
+    session.refresh(user)
+    
+    return user
+
+@router.get('/get_users')
+async def get_user(session: Session = Depends(get_session)):
+    users = session.exec(select(User)).all()
+
+    return users
