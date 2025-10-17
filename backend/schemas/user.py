3e5577f6b477de535eb3f812d1724a3615868e44
Obed Domínguez Mora <pacocobot2@gmail.com>
2025-10-17 15:05:03 -0600

    Create: UserCreate schema

---
+++ backend/schemas/user.py
@@ -0,0 +1,13 @@
+from sqlmodel import SQLModel
+from typing import Optional
+
+class UserBase(SQLModel):
+    name: str
+
+class UserCreate(UserBase):
+    surname: str
+    email: str
+    password: str
+    age: Optional[int] = None
+    weight: Optional[int] = None
+    height: Optional[int] = None
