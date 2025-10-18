b3e08596908abeb83e7e05450f5327afc5900e64
Obed Domínguez Mora <pacocobot2@gmail.com>
2025-10-17 23:07:32 -0600

    Create: register user endpoint & handler

--- backend/routes/routes.py
+++ backend/routes/routes.py
@@ -1,9 +1,13 @@
 #run with: uvicorn backend.app:app --reload --port 8000
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
+from sqlmodel import Session, select
+
 from database.connection import get_session
 from database.models.models import User, User_Role, Role
-from sqlmodel import Session, select
+from backend.schemas.user import UserCreate, UserRead
+
+from backend.services.user import register_user
 
 router = APIRouter()
 
@@ -15,13 +19,10 @@ async def root():
 async def hola():
     return {'hola': 12543}
 
-@router.post('/register_user')
-async def register_user(user: User, session: Session = Depends(get_session)):
-    session.add(user)
-    session.commit()
-    session.refresh(user)
-    
-    return user
+@router.post('/register_user', response_model=UserRead, status_code=201)
+def register_user_handler(data: UserCreate, session: Session = Depends(get_session)):
+    return register_user(data, session)
+
 
 @router.get('/get_users')
 async def get_user(session: Session = Depends(get_session)):
