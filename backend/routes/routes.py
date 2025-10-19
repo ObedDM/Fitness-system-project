07eb842ec0fd833bb2ed6a65b812baee19616a1e
Obed Domínguez Mora <pacocobot2@gmail.com>
2025-10-19 01:16:42 -0600

    Add: micronutrient endpoint

--- backend/routes/routes.py
+++ backend/routes/routes.py
@@ -3,14 +3,18 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlmodel import Session, select
 
+from fastapi.responses import PlainTextResponse
+
 from database.connection import get_session
 from database.models.models import User
 
 from backend.schemas.user import UserCreate, UserRead
 from backend.schemas.ingredients import IngredientCreate, IngredientRead
+from backend.schemas.micronutrient import MicronutrientCreate
 
 from backend.services.user import register_user
 from backend.services.ingredients import add_ingredient
+from backend.services.micronutrient import add_micronutrient
 
 router = APIRouter()
 
@@ -26,12 +30,17 @@ async def hola():
 def register_user_handler(data: UserCreate, session: Session = Depends(get_session)):
     return register_user(data, session)
 
-@router.post('/ingredient', response_model=str, status_code=201)
+@router.post('/ingredient', status_code=201)
 def add_ingredient_handler(data: IngredientCreate, session: Session = Depends(get_session)):
     
     ingredient = add_ingredient(data, session)
-    return f"ingredient {ingredient.name} created successfully. Id: {ingredient.ingredient_id}"
+    return {"message": f"ingredient {ingredient.name} created successfully. Id: {ingredient.ingredient_id}"}
 
+@router.post('/micronutrient', status_code=201)
+def add_micronutrient_handler(data: MicronutrientCreate, session: Session = Depends(get_session)):
+    
+    micronutrient = add_micronutrient(data, session)
+    return {"message": f"micronutrient {micronutrient.name} created successfully."}
 
 @router.get('/get_users')
 async def get_user(session: Session = Depends(get_session)):
