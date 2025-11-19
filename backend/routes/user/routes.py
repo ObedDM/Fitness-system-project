272020685a3b01735b0009838ba015a25f688ddc
Obed Domínguez Mora <pacocobot2@gmail.com>
2025-11-18 19:22:07 -0600

    Fix: added / to start of the route

--- backend/routes/user/routes.py
+++ backend/routes/user/routes.py
@@ -3,14 +3,18 @@ from sqlmodel import Session
 
 from database.connection import get_session
 from backend.routes.routes import router
-from backend.services.user.user import register_user, get_users
+from backend.services.user.user import register_user, get_users, get_profile
 from backend.schemas.user import UserCreate, UserRead
+from backend.utils.dependencies import get_user_id
 
-@router.post('user/register', response_model=UserRead, status_code=201)
+@router.post('/user/register', response_model=UserRead, status_code=201)
 async def register_handler(data: UserCreate, session: Session = Depends(get_session)):
     return register_user(data, session)
 
-@router.get('user/get_all', response_model=UserRead, status_code=201)
+@router.get('/user/get_all', response_model=UserRead, status_code=200)
 async def get_users_handler(session: Session = Depends(get_session)):
     return get_users(session)
 
+@router.get('/user/profile', response_model=UserRead, status_code=200)
+async def get_profile_handler(user_id: str = Depends(get_user_id), session: Session = Depends(get_session)):
+    return get_profile(user_id, session)
