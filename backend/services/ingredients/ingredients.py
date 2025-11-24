614e310e8807a5afae3c01998128c670cb2a808f
Obed Domínguez Mora <pacocobot2@gmail.com>
2025-11-24 14:57:36 -0600

    Update: Re-organized micronutrients and ignredient services into 1

--- backend/services/ingredients/ingredients.py
+++ backend/services/ingredients/ingredients.py
@@ -1,12 +1,11 @@
 from fastapi import HTTPException
 from sqlmodel import Session
+from sqlalchemy.exc import IntegrityError, SQLAlchemyError
 
 from database.models.models import Ingredient, Ingredient_MicroNutrient, MicroNutrient
 from backend.schemas.ingredients import IngredientCreate
-
+from backend.schemas.micronutrient import MicronutrientCreate
 from backend.utils.db_utils import is_existing
-
-from sqlalchemy.exc import IntegrityError, SQLAlchemyError
 
 def add_ingredient(data: IngredientCreate, session: Session) -> Ingredient:
 
@@ -49,5 +48,29 @@ def add_ingredient(data: IngredientCreate, session: Session) -> Ingredient:
     
     except Exception as e:
         # Unexpected errors
+        session.rollback()
+        raise HTTPException(500, f"Unexpected error: {str(e)}")
+    
+
+def add_micronutrient(data: MicronutrientCreate, session: Session) -> MicroNutrient:
+
+    try:
+        new_micronutrient = MicroNutrient(**data.model_dump())
+
+        session.add(new_micronutrient)
+        session.commit()
+        session.refresh(new_micronutrient)
+
+        return new_micronutrient
+    
+    except IntegrityError as e:
+        session.rollback()
+        raise HTTPException(409, f"Integrity error: {str(e.orig)}")
+
+    except SQLAlchemyError as e:
+        session.rollback()
+        raise HTTPException(500, f"database error: {str(e)}")
+    
+    except Exception as e:
         session.rollback()
         raise HTTPException(500, f"Unexpected error: {str(e)}")
